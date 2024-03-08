f4ce94b0460b865dc2dee17e314986e0b17eab77
1F10220182 <s1f102201829@iniad.org>
2024-03-08 22:24:51 +0900

    made template

---
+++ demo-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import itemList from './itemList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <itemList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ demo-app/src/itemList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "./App";
+
+const itemList = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default itemList;
