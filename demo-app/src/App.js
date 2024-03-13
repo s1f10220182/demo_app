064cbddf84020d41db646de03a666a12eb47894a
s1f10220182 <107394748+s1f10220182@users.noreply.github.com>
2024-03-13 21:28:32 +0900

    Revert "一覧から詳細ページへ飛ぶ開発"

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -2,19 +2,13 @@ import React from 'react';
 // import ItemList from './itemList';
 import Header from './header/Header';
 import Main from './main/Main';
-import Detail from './func/Detail';
 import Footer from './footer/Footer';
-import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='App'>
+    <div>
       <Header/>
-      <Routes>
-        <Route path='/' element={<Main/>}/>
-        <Route path='/detail' element={<Detail/>}/>
-        {/*<Route path='*' element={<Notfound/>}/>*/}
-      </Routes>
+      <Main/>
       <Footer/>
     </div>
   );
